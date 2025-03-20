844aecd4abbad1e7a7d74a88d4eed179b7034d0a
André Luis Fernandes <62942316+Andre-StudioNerd@users.noreply.github.com>
2025-03-20 15:39:00 -0300

    Adicionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import Login from "./components/login/login";
+import Login from "./components/login/Index.jsx";
+
 
 function App() {
   return (
     <div className="App">
-     <Login />
+     <Login/>
     </div>
   );
 }
